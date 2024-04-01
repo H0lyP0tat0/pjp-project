7275c42ef990c4d8ef5290d02475ec11be15545e
H0lyP0tat0 <95308667+H0lyP0tat0@users.noreply.github.com>
2024-04-01 21:55:19 +0200

    First lesson

---
+++ AntlrTest6.0/MFL.g4
@@ -0,0 +1,50 @@
+grammar MFL;
+
+prog: statement* EOF;
+
+expr: '-' expr
+    | '!' expr 
+    | expr ('*'|'/'|'%') expr
+    | expr ('+'|'-'|'.') expr
+    | expr ('<'|'>') expr
+    | expr ('=='|'!=') expr
+    | expr '&&' expr
+    | expr '||' expr
+    | ID '=' expr
+    | ID
+    | INT                   
+    | FLOAT                 
+    | BOOL                  
+    | STRING                
+    | '(' expr ')'          
+    ;
+
+INT : [1-9][0-9]* | [0] ;
+FLOAT : [0-9]+[.][0-9]* ;
+BOOL : 'true' | 'false' ;
+STRING : '"' CHARSEQUENCE* '"' ;
+ID : [a-zA-Z] [a-zA-Z0-9]* ;
+WS : [ \t\r\n]+ -> skip ;
+COMMENT : '//' ~[\r\n]* -> skip ;
+
+// types
+type : 'int' | 'string' | 'float' | 'bool' ;
+
+// statements
+statement
+    : type ID (',' ID)* ';'
+    | expr ';'
+    | 'read' ID (',' ID)* ';'
+    | 'write' expr (',' expr)* ';'
+    | '{' statement+ '}'
+    | 'if' '(' expr ')' statement ('else' statement)?
+    | 'while' '(' expr ')' statement
+    | ';'
+    ;
+    
+fragment CHARSEQUENCE
+    : ~["\\\r\n]
+    | '\\' ['"?abfnrtv\\]
+    | '\\\n'   // Added line
+    | '\\\r\n' // Added line
+    ;
---
+++ AntlrTest6.0/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Globalization;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+using AntlrTest;
+
+namespace AntlrTest6._0
+{
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            var fileName = "input.txt";
+            Console.WriteLine("Parsing: " + fileName);
+            var inputFile = new StreamReader(fileName);
+            AntlrInputStream input = new AntlrInputStream(inputFile);
+            MFLLexer lexer = new MFLLexer(input);
+            CommonTokenStream tokens = new CommonTokenStream(lexer);
+            MFLParser parser = new MFLParser(tokens);
+
+            parser.AddErrorListener(new VerboseListener());
+
+            IParseTree tree = parser.prog();
+
+            if (parser.NumberOfSyntaxErrors == 0)
+            {
+                Console.WriteLine(tree.ToStringTree(parser));
+                // ParseTreeWalker walker = new ParseTreeWalker();
+                // walker.Walk(new EvalListener(), tree);
+
+                // new EvalVisitor().Visit(tree);
+            }
+        }
+    }
+}
